53e95a67588bb5f2b22f1ab19131934f0e03afe8
vasanthlmsace <vasanthr.lmsace@gmail.com>
2021-09-21 15:05:59 +0530

    updated latest source

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021091601;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2021092101;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500;         // Requires this Moodle version.
-$plugin->release = 'Version 1.1.9';
+$plugin->release = 'Version 1.2';
 $plugin->component = 'format_kickstart'; // Full name of the plugin (used for diagnostics).
 $plugin->supported = [39, 311];
 $plugin->maturity  = MATURITY_STABLE;
